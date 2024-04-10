31482059b45a3af3652287b6ac4531ec26698070
Zerouali Hamza <zrewlate.hamza11@gmail.com>
2024-04-10 17:03:56 -0400

    Implémentation de l'interface graphique du projet

---
+++ Rechercher_disponibilite.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gestion_des_horaires
+{
+    /// <summary>
+    /// Interaction logic for Rechercher_disponibilite.xaml
+    /// </summary>
+    public partial class Rechercher_disponibilite : Window
+    {
+        public Rechercher_disponibilite()
+        {
+            InitializeComponent();
+        }
+
+        private void Retour_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Créez une nouvelle instance de votre fenêtre de menu
+            Menu menuWindow = new Menu();
+
+            // Affichez la fenêtre de menu
+            menuWindow.Show();
+
+            // Fermez la fenêtre de disponibilité
+            this.Hide();
+        }
+    }
+}
